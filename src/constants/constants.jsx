de6dccd5d378919141905bb16e25f790452763dd
Muhammed Fayis P <mhdfaaiz803@gmail.com>
2024-08-18 12:01:48 +0530

    api key changes

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,3 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3';
-export const API_KEY = '6031bbf9c239cc44f8299082b16a56c5';
+export const API_KEY = 'e2cb2c64e135bdf3fc661352dcf176ec';
 export const imageUrl = 'https://image.tmdb.org/t/p/original'
